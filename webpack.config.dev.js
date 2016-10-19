0576306be388edbf916f4872f48256596f1cb636
mhd999 <m.almouallem@gmail.com>
2016-10-19 15:43:09 +0200

    Chore: add eslint command to package.json

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,29 +2,29 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  debug: true, //enable displaying debug information
-  devtool: 'inline-source-map', //check the documentation for other devtool options
-  noInfo: false, //webpack not show files are bundling
+  debug: true, // enable displaying debug information
+  devtool: 'inline-source-map', // check the documentation for other devtool options
+  noInfo: false, // webpack not show files are bundling
   entry: [
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/app')
+    'webpack-hot-middleware/client?reload=true', // note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'src/app'),
   ],
   target: 'web',
   output: {
-    path: __dirname + '/build', // Note: Physical files are only output by the production build task `npm run build`.
+    path: path.resolve(__dirname, '/build'), // Note:Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'src')
+    contentBase: path.resolve(__dirname, 'src'),
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']}
-    ]
-  }
+      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
+    ],
+  },
 };
